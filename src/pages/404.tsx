8273daa63a4fc770ff7ddee2b91b3f55acab2b52
dulguundok0912 <108997100+dulguundok0912@users.noreply.github.com>
2022-07-17 01:54:47 +0800

    Add files via upload

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,21 @@ import { Layout } from "../components/layout";
 
 export const NotFound: FunctionComponent = () => {
   return (
-    <Layout>
-      <h1 className="text-3xl text-center my-6">UUCHIL :l</h1>
-      <img className="max-w-5xl mx-auto my-6" src="/logo.png"></img>
-    </Layout>
+    ---
+    template: blog-post
+    title: Өдрийн онцлох мэдээ!!!
+    publishedDate: 2022-07-15T12:00:28.345Z
+    description: Орон нутгийн замд хурдаа тохируулан оролцохыг цагдаагийн байгууллага анхааруулж байна
+    featured: true
+    img: ../../static/images/машаан.png
+    imgAlt: Тухайн ослын зураг
+    tags:
+      - Цагдаа
+      - Үхэл
+      - Осол
+    ---
+    2022 оны долдугаар сарын 13-ны 11:05 цагт .Саното-8, Т.Highlander автомашинууд хурдаа тохируулаагүйн улмаас 3 хүн газар дээрээ нас баржээ.
+    Та бүхэн орон нутгын замд хурдаа тохируулан явахыг цагдаагын байгууллага анхааруулж байна!
   );
 };
 
