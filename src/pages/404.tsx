448eb5fa1f8b0cc1a6cbe6e45730f980cd4afc77
dulguundok0912 <108997100+dulguundok0912@users.noreply.github.com>
2022-07-17 01:47:49 +0800

    Add files via upload

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,8 @@ import { Layout } from "../components/layout";
 export const NotFound: FunctionComponent = () => {
   return (
     <Layout>
-      <h1 className="text-3xl text-center my-6">Not Found</h1>
-      <img className="max-w-5xl mx-auto my-6" src="/404.jpeg"></img>
+      <h1 className="text-3xl text-center my-6">UUCHIL :l</h1>
+      <img className="max-w-5xl mx-auto my-6" src="/logo.png"></img>
     </Layout>
   );
 };
